650c8abec52cb3a6f55afcc2f7103df9689bc8de
tayjee <jimmy.tang.360@gmail.com>
2022-03-01 07:21:53 -0500

    Add syntax consistency

--- src/components/Application.js
+++ src/components/Application.js
@@ -52,7 +52,7 @@ export default function Application(props) {
     appointments: {}
   });
 
-  const setDay = day => setState({ ...state, day });
+  const setDay = (day) => setState({ ...state, day });
   const setDays = (days) => {
     setState(prev => ({ ...prev, days }));
 }
