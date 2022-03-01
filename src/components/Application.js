351aff28280ec37c3b0205f83f538cdb93f5cc1e
tayjee <jimmy.tang.360@gmail.com>
2022-03-01 07:09:15 -0500

    Implement setDay && setDays and worked with state management

--- src/components/Application.js
+++ src/components/Application.js
@@ -4,50 +4,58 @@ import DayList from "./DayList";
 import Appointment from "components/Appointment";
 import "components/Application.scss";
 
-const appointments = [
-  {
-    id: 1,
-    time: "12pm",
-  },
-  {
-    id: 2,
-    time: "1pm",
-    interview: {
-      student: "Lydia Miller-Jones",
-      interviewer:{
-        id: 3,
-        name: "Sylvia Palmer",
-        avatar: "https://i.imgur.com/LpaY82x.png",
-      }
-    }
-  },
-  {
-    id: 3,
-    time: "2pm",
-  },
-  {
-    id: 4,
-    time: "3pm",
-    interview: {
-      student: "Archie Andrews",
-      interviewer:{
-        id: 4,
-        name: "Cohana Roy",
-        avatar: "https://i.imgur.com/FK8V841.jpg",
-      }
-    }
-  },
-  {
-    id: 5,
-    time: "4pm",
-  }
-];
-
-
 export default function Application(props) {
 
-  const [day, setDay] = useState("Monday");
-  const [days, setDays] = useState([]);
+  // const appointments = [
+  //   {
+  //     id: 1,
+  //     time: "12pm",
+  //   },
+  //   {
+  //     id: 2,
+  //     time: "1pm",
+  //     interview: {
+  //       student: "Lydia Miller-Jones",
+  //       interviewer:{
+  //         id: 3,
+  //         name: "Sylvia Palmer",
+  //         avatar: "https://i.imgur.com/LpaY82x.png",
+  //       }
+  //     }
+  //   },
+  //   {
+  //     id: 3,
+  //     time: "2pm",
+  //   },
+  //   {
+  //     id: 4,
+  //     time: "3pm",
+  //     interview: {
+  //       student: "Archie Andrews",
+  //       interviewer:{
+  //         id: 4,
+  //         name: "Cohana Roy",
+  //         avatar: "https://i.imgur.com/FK8V841.jpg",
+  //       }
+  //     }
+  //   },
+  //   {
+  //     id: 5,
+  //     time: "4pm",
+  //   }
+  // ];
+
+  const [state, setState] = useState({
+    day: "Monday",
+    days: [],
+    // you may put the line below, but will have to remove/comment hardcoded appointments variable
+    appointments: {}
+  });
+
+  const setDay = day => setState({ ...state, day });
+  const setDays = (days) => {
+    setState(prev => ({ ...prev, days }));
+}
 
   useEffect(() => {
     axios.get("/api/days")
@@ -67,7 +75,11 @@ export default function Application(props) {
 />
 <hr className="sidebar__separator sidebar--centered" />
 <nav className="sidebar__menu">
-<DayList days={days} value={day} onChange={setDay} />
+<DayList 
+days={state.days} 
+value={state.day} 
+onChange={setDay} 
+/>
 </nav>
 <img
   className="sidebar__lhl sidebar--centered"
